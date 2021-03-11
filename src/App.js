a55345926c725f8c01c867bc998bbe4ed00eb2d8
Diego Maia <diego.maia@montreal.com.br>
2021-03-11 11:45:02 -0300

    Commit 7: Código exemplo de classes com react mais código exemplo no README

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo}
-            idade={props.idade}/>
-            <Social facebook={props.facebook}/>
-        </div>
-    );
+class Equipe extends Component {
+    render(){
+        return (
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo}
+                 idade={this.props.idade} />
+                <hr/>
+            </div>
+        );
+    }
 }
 
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h2>Cargo: {props.cargo}</h2>
-            <he>Idade: {props.idade}</he>
-            <hr/>
-        </div>
-    );
-}
-
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.facebook}> Facebook </a>
-            <a>LinkedIn </a>
-            <a>YouTube </a>
-        </div>
-    );
+class Sobre extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Olá, sou: {this.props.nome}</h1>
+                <h2>Cargo: {this.props.cargo}</h2>
+                <h2>idade: {this.props.idade}</h2>
+            </div>
+        );
+    }
 }
 
 function App(){
     return (
         <div>
-            <h1>Curso React</h1>
-            <Equipe nome="Lucas" cargo="Programador" idade="29"
-            facebook="https://google.com"/>
-            <Equipe nome="Lucas" cargo="Designer" idade="22"/>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Matheus" cargo="Desenvolvedor" idade="24"/>
+            <Equipe nome="Lucas" cargo="Designer" idade="21"/>
         </div>
     );
 }
--- src/README.md
+++ src/README.md
@@ -1,4 +1,38 @@
 # Sobre esse projeto
 ### Se estiver aqui para estudar :point_down:
 > _**Props**_:
-> - A Utilização de props, serve para receber valores e chamar funções que desempenharão comportamentos. Os _**commits 4 e 5**_ possuem exemplos de código somente sobre esse assunto.
+> - A Utilização de props, serve para receber valores e chamar funções que desempenharão comportamentos. Os _**commits 5 e 6**_ possuem exemplos de código somente sobre esse assunto.
+
+> - _**Classes**_:
+> _Exemplo de código:_ _**commits 7**_.
+> - Classes são o uso mais comum do que funções. Bem parecido com o Java. 
+> Todo retorno de classes (e até mesmo de funções) precisa ter uma tag pai. Caso contrário haverá um erro.
+> - Exemplo de código _**correto**_ :point_down:
+> ~~~javascript
+> class  Sobre  extends  Component{
+> 		render(){
+>			return(
+>				<div>
+>					<h1>Olá, sou: {this.props.nome}</h1>
+>					<h2>Cargo: {this.props.cargo}</h2>
+>					<h2>idade: {this.props.idade}</h2>
+>				</div>
+>			);
+>		}
+> }
+> ~~~
+> - Exemplo de código _**errado**_ :point_down:
+>  ~~~javascript
+> class  Sobre  extends  Component{
+> 		render(){
+>			return(
+>				<div>
+>					<h1>Olá, sou: {this.props.nome}</h1>
+>					<h2>Cargo: {this.props.cargo}</h2>
+>				</div>
+> 				<h2>idade: {this.props.idade}</h2>
+>			);
+>		}
+> }
+> ~~~
+> - A tag **h2** que contem o props **idade** não possui tag pai. Se ela fosse a única dentro dessa classe então então a tag dela sera a pai. 
